3438e954bc3f9299e0fecb532122f581c798eb2d
Lauda Carly <lauda@nel.re>
2025-10-04 15:04:26 +0200

    Simplify

--- examples/empty/main.rs
+++ examples/empty/main.rs
@@ -2,10 +2,8 @@ use cen::app::app::{App, AppConfig};
 use cen::app::component::ComponentRegistry;
 
 fn main() {
-    let registry = ComponentRegistry::new();
-
     App::run(
         AppConfig::default(),
-        registry,
+        ComponentRegistry::new(),
     );
 }
