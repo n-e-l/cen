e4894285e42bda6d420509db5a84e8cfbe3ebec9
Lauda Carly <lauda@nel.re>
2026-05-01 19:49:05 +0200

    Add logical device test

--- src/vulkan/device.rs
+++ src/vulkan/device.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 use ash::khr::swapchain;
-use ash::vk;
+use ash::{vk, Entry};
 use ash::vk::{PipelineStageFlags, Queue};
 use log::trace;
 use crate::vulkan::{CommandBuffer, Instance, LOG_TARGET};
@@ -183,4 +183,18 @@ impl Device {
             inner: self.inner.clone(),
         }
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create_logical_device() {
+        let entry = Entry::linked();
+        let instance = Instance::new(&entry, None);
+        let (physical_device, queue_family_index) = instance.create_physical_device_headless();
+        let _device = Device::new(&instance, physical_device, queue_family_index);
+    }
+
 }
